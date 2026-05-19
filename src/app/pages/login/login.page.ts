616cb7ed97d9f3cea10e0ccbc03b4bd8e8c00854
Luis <luisyagual2010@gmail.com>
2026-05-18 19:38:30 -0500

    first commit

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {} 
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  registrar() {
+    if (this.loginForm.valid) {
+      console.log('Datos del formulario:', this.loginForm.value);
+
+
+      this.router.navigateByUrl('/home');
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
+}
